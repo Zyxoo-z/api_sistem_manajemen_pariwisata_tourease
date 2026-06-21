95cca518fc756d72ded166fced6c0b3bcbfbe3cb
EL Zyxo <unknown515partyon@gmail.com>
2026-06-22 01:02:33 +0800

    Kiky
    Db Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,24 +3,18 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        User::updateOrCreate(
-            [
-                'email' => 'test@example.com'
-            ],
-            [
-                'name' => 'Test User',
-                'password' => Hash::make('test123'),
-            ]
-        );
+        $this->call([
+            UserSeeder::class,
+            DestinasiSeeder::class,
+            PaketSeeder::class,
+            BookingSeeder::class,
+            ReviewSeeder::class,
+            PaymentSeeder::class,
+        ]);
     }
 }
